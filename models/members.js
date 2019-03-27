c82d34d93ed2f5cb714e3e6e8a5eca3a8efa1dc6
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-03-27 14:58:13 +0300

    remove email from model

--- models/members.js
+++ models/members.js
@@ -16,10 +16,6 @@ let memberSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    email: {
-        type: String,
-        required: true
-    },
     password: {
         type: String,
         required: true
