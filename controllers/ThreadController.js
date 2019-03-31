b1a710f8aaeaa554bf0983855908c495b7a27675
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-03-31 13:55:14 +0300

    create thread controller

---
+++ controllers/ThreadController.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const Thread = require('../models/threads');
+
+module.exports = {
+    index: function (req, res, next) {
+        Thread.find({})
+        .exec()
+        .then(threads => {
+            console.log(threads);
+            res.status(200).json(threads);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+    },
+    store: function (req, res, next) {
+        let thread = new Thread();
+        thread.title=req.body.title;
+        thread.body=req.body.body;
+        return thread.save()
+        .then(threads => {
+            console.log(threads);
+            res.status(200).json(threads);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+    },
+    edit: function (req, res, next) {
+
+    },
+    update: function (req, res, next) {
+
+    },
+    delete: function (req, res, next) {
+
+    }
+}
