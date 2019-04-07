381478a6022cb5dbe17c502645641009d16e2f5e
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-07 23:48:33 +0300

    add function to retrieve all comments

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,4 +1,14 @@
-const Forum = require('../models/comments');
+const Comment = require('../models/comments');
 module.exports = {
-    
+    index: function (req, res, next) {
+        Comment.find({})
+        .exec()
+        .then(comments => {
+            console.log(comments);
+            res.status(200).json(comments);
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+    },
 }
