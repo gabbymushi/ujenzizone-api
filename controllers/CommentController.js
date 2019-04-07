b956d55ec853c9f18fffa425da74c451307a6a5d
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-07 23:53:53 +0300

    handle  errors when saving comments

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -20,7 +20,9 @@ module.exports = {
             console.log(comments);
             res.status(200).json(comments);
         })
-        
+        .catch(err => {
+            res.status(500).json(err)
+        });
       
     },
 }
