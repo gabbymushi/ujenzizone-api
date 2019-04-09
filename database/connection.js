084bf18dd6c746ab92b4a22d39b6f0668978519b
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-09 19:04:10 +0300

    export sequelize globally

--- database/connection.js
+++ database/connection.js
@@ -1,3 +1,4 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("ujenzizone", "root", "gabby", { host: '127.0.0.1', dialect: 'mysql', operatorsAliases: false });
 module.exports = sequelize;
+global.sequelize=sequelize;
