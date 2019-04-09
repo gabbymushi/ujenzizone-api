73c39fd2602d6bbd10bdd377ca57498b65f9ec38
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-09 19:47:09 +0300

    create table meber schema in migration

---
+++ migrations/20190409163830-create_table_members.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.createTable('members', {
+      member_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      first_name: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING(60),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true
+      },
+      password:{
+        type:Sequelize.STRING(255),
+        allowNull:false
+    },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
