3b23d3ec1a171c59a76fec13c3496d96cda26b03
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-09 19:28:37 +0300

    add column member id in member model

--- models/Member.js
+++ models/Member.js
@@ -1,1 +1,10 @@
 const Sequelize=require("sequelize");
+module.exports=sequelize.define("Member",{
+    member_id:{
+        type:Sequelize.BIGINT,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true
+    }
+
+});
