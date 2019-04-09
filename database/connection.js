31dd46f0a99f9e6a4c465a1f68e2feb22a180a53
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-09 20:31:14 +0300

    rename config.js to _config.js

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,18 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("ujenzizone", "root", "gabby", { host: '127.0.0.1', dialect: 'mysql', operatorsAliases: false });
+const sequelize = new Sequelize(
+    "ujenzizone",
+    "root",
+    "gabby",
+    {
+        host: '127.0.0.1',
+        dialect: "mysql",
+        operatorsAliases: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
 module.exports = sequelize;
-global.sequelize=sequelize;
+global.sequelize = sequelize;
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const config = require('./config/config');
+const config = require('./config/_config');
 const jwt = require('jsonwebtoken');
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/ujenzizone');
 //let db = mongoose.connection;
  require('./database/connection');
 //check connection
-db.once('open', function () {
-    console.log('Connected to mangodb');
-});
+// db.once('open', function () {
+//     console.log('Connected to mangodb');
+// });
 //check for DB errors
-db.on('error', function (err) {
-    console.log(err)
-});
+// db.on('error', function (err) {
+//     console.log(err)
+// });
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,31 +21,31 @@ app.use(express.json());
 //let Member = require('./models/members')
 
 //route files
-let member = require('./routes/members');
-let forum = require('./routes/forum');
-let thread = require('./routes/threads');
-let auth = require('./routes/auth');
-let comment = require('./routes/comment');
-app.use('/api/v1/auth', auth);
-app.use('/api/v1/members',validateUser,member);
-app.use('/api/v1/forums',validateUser,forum);
-app.use('/api/v1/comments',validateUser,comment);
-//app.use('/api/v1/forums',forum);
-app.use('/api/v1/threads',validateUser,thread);
-function validateUser(req, res, next) {
-    //res.json({status:"error",data:req.headers['x-access-token']});
-    var token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token,config.secret, function(err, decoded) {
-      if (err) {
-        res.status(403).json({status:"error", message: err.message, data:null});
-      }else{
-        // add user id to request
-        req.body.userId = decoded.id;
-        //res.json({status:"error", data:decoded.id});
-        next();
-      }
-    });
+// let member = require('./routes/members');
+// let forum = require('./routes/forum');
+// let thread = require('./routes/threads');
+// let auth = require('./routes/auth');
+// let comment = require('./routes/comment');
+// app.use('/api/v1/auth', auth);
+// app.use('/api/v1/members',validateUser,member);
+// app.use('/api/v1/forums',validateUser,forum);
+// app.use('/api/v1/comments',validateUser,comment);
+// //app.use('/api/v1/forums',forum);
+// app.use('/api/v1/threads',validateUser,thread);
+// function validateUser(req, res, next) {
+//     //res.json({status:"error",data:req.headers['x-access-token']});
+//     var token = req.headers.authorization.split(' ')[1];
+//     jwt.verify(token,config.secret, function(err, decoded) {
+//       if (err) {
+//         res.status(403).json({status:"error", message: err.message, data:null});
+//       }else{
+//         // add user id to request
+//         req.body.userId = decoded.id;
+//         //res.json({status:"error", data:decoded.id});
+//         next();
+//       }
+//     });
     
-  }
+//   }
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log('Ujenzi zone is running on port ' + port + '...'));
