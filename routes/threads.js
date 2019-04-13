fb288e24a51cc4a9f2978ea7783141027f2c47b9
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-13 18:13:38 +0300

    add get thread based on forum id end point in thread routes

--- routes/threads.js
+++ routes/threads.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
 let threadController = require('../controllers/ThreadController');
-router.get('/',threadController.index);
+router.get('/:id',threadController.index);
 router.post('/',threadController.store);
 module.exports=router;
