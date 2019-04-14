602025d81c066133ab111b0c6324fafa99d4d986
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-15 00:18:03 +0300

    import thread model

--- controllers/ThreadController.js
+++ controllers/ThreadController.js
@@ -1,33 +1,37 @@
-const express = require('express');
 const Thread = require('../models/Thread');
+const Forum = require('../models/Forum');
 
 module.exports = {
     index: function (req, res, next) {
-       //return res.status(200).json(req.params.id)
-        Thread.findAll({where: {
-            forum_id: req.params.id
-        }})
-        .then(threads => {
-            console.log(threads);
-            res.status(200).json(threads);
+        //return res.status(200).json(req.params.id)
+        Thread.findAll({
+            where: {
+                forum_id: req.params.id
+            },
+            
         })
-        .catch(err => {
-            res.status(500).json(err)
-        });
+         .then(threads => {
+                console.log(threads);
+                res.status(200).json(threads);
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json(err)
+            });
     },
     store: function (req, res, next) {
         let thread = new Thread();
-        thread.title=req.body.title;
-        thread.body=req.body.body;
-        thread.forum_id=req.body.forum_id;
+        thread.title = req.body.title;
+        thread.body = req.body.body;
+        thread.forum_id = req.body.forum_id;
         return thread.save()
-        .then(threads => {
-            console.log(threads);
-            res.status(200).json(threads);
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        });
+            .then(threads => {
+                console.log(threads);
+                res.status(200).json(threads);
+            })
+            .catch(err => {
+                res.status(500).json(err)
+            });
     },
     edit: function (req, res, next) {
 
