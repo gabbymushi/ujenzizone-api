1aabdb3c09635ce907c522f4dcd736cdd84778ca
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-15 00:19:25 +0300

    eager load forum model with alias forum

--- controllers/ThreadController.js
+++ controllers/ThreadController.js
@@ -10,7 +10,7 @@ module.exports = {
             },
             include: [{
                 model:Forum,
-                
+                as: 'forum'
             }]
         })
          .then(threads => {
