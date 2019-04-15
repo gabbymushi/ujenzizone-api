2d18ecbd5cda3a07d9acb208c218f45746a500d4
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-15 07:23:56 +0300

    add comment id

--- migrations/20190415041926-create_table_comment.js
+++ migrations/20190415041926-create_table_comment.js
@@ -9,6 +9,15 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
+    return queryInterface.createTable('threads', {
+      comment_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+    
+    });
   },
 
   down: (queryInterface, Sequelize) => {
