9038ba13a0b5022035a80ac7ef09b7e08b4af1bc
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-15 07:06:42 +0300

    create comment id field

--- models/Comment.js
+++ models/Comment.js
@@ -2,4 +2,10 @@ const Sequelize=require("sequelize");
 const sequelize=require("../database/connection");
 const Thread = require('../models/Thread');
 const Comment=sequelize.define("Comment",{
+    comment_id:{
+        type:Sequelize.BIGINT,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true
+    },
 });
