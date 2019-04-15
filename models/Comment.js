564a90528aec24e35f453c6a064fc1db42367f70
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-15 07:05:38 +0300

    sequelize.define model

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,5 @@
 const Sequelize=require("sequelize");
 const sequelize=require("../database/connection");
 const Thread = require('../models/Thread');
+const Comment=sequelize.define("Comment",{
+});
