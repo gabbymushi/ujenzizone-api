5e6b7cf3067b8e5c147d12e6f74cd312003014aa
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-19 13:39:40 +0300

    make db name stat with small letter

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const Sequelize=require("sequelize");
 const sequelize=require("../database/connection");
 const Thread = require('../models/Thread');
-const Comment=sequelize.define("Comment",{
+const Comment=sequelize.define("comment",{
     comment_id:{
         type:Sequelize.BIGINT,
         allowNull:false,
