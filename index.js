9f621a94b2563de02e8ae53d42385b86634496de
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-04-22 14:25:03 +0300

    impot http sockect.io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
 const cors = require('cors');
 const config = require('./config/_config');
 const jwt = require('jsonwebtoken');
