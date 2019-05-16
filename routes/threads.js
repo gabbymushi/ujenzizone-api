6ddcd2b1a5efc4e84d69bfb68d4688724391f2c1
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-05-16 13:20:29 +0300

    change thread and forum routes

--- routes/threads.js
+++ routes/threads.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 let threadController = require('../controllers/ThreadController');
-router.get('/:id/:offset',threadController.index);
-router.get('/:id/thread',threadController.getThreadById);
+router.get('/:id/offset/:offset',threadController.index);
+router.get('/:id/',threadController.getThreadById);
 router.post('/',threadController.store);
 module.exports=router;
