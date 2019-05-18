eef0eed4cabe19e6e88fb26e0ff5548d348ce1cf
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-05-18 11:41:43 +0300

    add file name attrib

--- migrations/20190518072941-create_tbl_files.js
+++ migrations/20190518072941-create_tbl_files.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -9,31 +9,35 @@ module.exports = {
       Example:
       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
     */
-   return queryInterface.createTable('files', {
-    file_id: {
-      type: Sequelize.BIGINT,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true
-    },
-    thread_id: {
-      type: Sequelize.BIGINT,
-      allowNull: true,
-      //index: true,
-      references: {
-         model: 'threads', 
-         key: 'thread_id' 
+    return queryInterface.createTable("files", {
+      file_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      file_name: {
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      thread_id: {
+        type: Sequelize.BIGINT,
+        allowNull: true,
+        //index: true,
+        references: {
+          model: "threads",
+          key: "thread_id"
         },
-        onUpdate: 'cascade',
-        onDelete: 'cascade'
-    },
-    mime_type:{
-      type:Sequelize.STRING(20),
-      allowNull:false
-  },
-  createdAt: Sequelize.DATE,
-  updatedAt: Sequelize.DATE,
-  });
+        onUpdate: "cascade",
+        onDelete: "cascade"
+      },
+      mime_type: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    });
   },
 
   down: (queryInterface, Sequelize) => {
