bde9142e70cc80ac46ead2f5e03b390f8978880b
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-05-18 10:56:47 +0300

    add file id attrib in model

--- models/File.js
+++ models/File.js
@@ -4,7 +4,12 @@ const Forum = require('../models/Forum');
 const Member = require('../models/Member');
 
 const File=sequelize.define("file",{
-
+    file_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
   
 });
 
