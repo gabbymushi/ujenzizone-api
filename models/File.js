6d45a4150149c9aab085856fae100efee9c1ebee
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-05-18 10:56:05 +0300

    create file model

---
+++ models/File.js
@@ -0,0 +1,11 @@
+const Sequelize=require("sequelize");
+const sequelize=require("../database/connection");
+const Forum = require('../models/Forum');
+const Member = require('../models/Member');
+
+const File=sequelize.define("file",{
+
+  
+});
+
+  module.exports= Thread;
