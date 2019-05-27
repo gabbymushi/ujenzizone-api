52e1eca04651ecc3b30c1996b13110b7b0487544
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-05-27 21:20:02 +0300

    get filename from the array

--- controllers/ThreadController.js
+++ controllers/ThreadController.js
@@ -56,7 +56,7 @@ module.exports = {
         // res.status(200).json(threads);
         for(var i=0;i<req.files.lenght;i++){
         let file = new File();
-        file.file_name = req.file.filename;
+        file.file_name = req.file[i].filename;
         file.mime_type = req.file.mimetype;
         file.thread_id = threads.thread_id;
         file.save();
