63669e7dafb134871ac209238f1d83ecf9adea7c
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-04 13:53:41 +0300

    remove deprecated Operatoraliases

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     {
         host: '127.0.0.1',
         dialect: "mysql",
-        operatorsAliases: false,
+        // operatorsAliases: true,
         pool: {
             max: 5,
             min: 0,
